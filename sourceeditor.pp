076fa94b9a7adb415ca3c996d4d5489126184861
Lezsák Domonkos <lezsakdomi1@gmail.com>
2015-01-31 17:47:03 +0100

    Imported the editor

---
+++ sourceeditor.pp
@@ -0,0 +1,30 @@
+unit sourceeditor;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { Tsourceeditor }
+
+  Tsourceeditor = class(TForm)
+    Button1: TButton;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  sourceeditor: Tsourceeditor;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
